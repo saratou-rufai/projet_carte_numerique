84031003cbe1053f20d551595822b0fd5379d4d4
Saratou Rufai <saratou664@gmail.com>
2026-01-19 15:00:14 +0000

    Premier envoi du projet Carte Numerique

---
+++ database/migrations/2026_01_19_002953_create_cartes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cartes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('etudiant_id')->constrained('etudiants')->onDelete('cascade');
+            $table->string('numero_carte')->unique();
+            $table->string('qr_code')->unique();
+            $table->enum('statut', ['active', 'suspendue', 'expiree'])->default('active');
+            $table->date('date_creation');
+            $table->date('date_expiration');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cartes');
+    }
+};
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+<body>
+ <h1>bonjour</h1>
+</body>
+</html>
