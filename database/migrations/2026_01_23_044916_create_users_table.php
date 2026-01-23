25fb03d4813b5bbb39137d9794ec3ff0826fbc49
DAYAMBA Blaise <dyblaise012@gmail.com>
2026-01-23 15:26:33 +0000

    Suppression de du fichier api.php

---
+++ database/migrations/2026_01_23_044916_create_users_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom', 100);
+            $table->string('prenom', 100);
+            $table->string('email', 150)->unique();
+            $table->string('password');
+            $table->enum('role', ['admin'])->default('admin');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
